bff678e7ea089496951ef94f9acba6089edeeea4
Edy Gunawan <gunawanedy40@gmail.com>
2021-03-29 11:08:50 +0700

    Added template for easier debug

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './index';
+
+ReactDOM.render(
+  <>
+    <Index>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+      <button style={{ padding: '20px 70px' }}>test</button>
+    </Index>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, harum
+      sequi? Voluptatem, cum accusantium? Vel quasi et quam dolor inventore
+      culpa dolorem possimus quas fuga, ducimus nesciunt a, cumque ratione
+      voluptatibus commodi ab temporibus itaque, cupiditate cum minus. Incidunt
+      consequatur debitis culpa quod, maxime a doloremque eum quo neque
+      perspiciatis sed fuga nulla libero aut quia quos corrupti. Repellat nobis
+      veritatis, earum voluptatibus non rerum numquam iusto officia cum,
+      voluptate, corporis distinctio similique doloribus atque! At ipsam, illo
+      sed dolorum saepe veritatis molestiae, tempora id esse, quasi natus non
+      molestias nisi consequuntur eaque consectetur blanditiis fugit tenetur
+      deserunt! Tenetur illo laboriosam iste autem, quidem laudantium aliquid
+      nobis illum. Doloremque ex amet totam adipisci minima mollitia voluptatum
+      voluptatibus fugiat quisquam odio quibusdam, itaque nostrum facere veniam
+      aperiam voluptatem? Laboriosam fuga vitae laborum magni accusantium
+      dolorum odit, doloribus expedita cumque aut corrupti explicabo aspernatur
+      inventore laudantium modi voluptates sint, molestiae beatae quibusdam.
+      Rerum quam minus deleniti dolorum neque eos atque, at cumque, quisquam
+      ipsum molestiae ad, laudantium ex sunt qui expedita odit. Dolore, illo
+      inventore! Impedit sapiente aliquam, corporis rerum ipsum voluptas
+      reiciendis numquam cumque minus nihil molestiae aliquid iusto harum non
+      inventore incidunt, corrupti fugit, ratione ullam! Ratione iusto ea, amet
+      odio laudantium praesentium error dolores exercitationem deserunt nam
+      inventore! Dolore temporibus reiciendis rerum illo aliquid, cupiditate
+      ipsum molestiae voluptatibus soluta blanditiis corrupti fugit iusto aut
+      expedita, corporis qui laboriosam provident dolores natus repellendus,
+      unde neque magni minima. Molestiae eaque iste delectus, vel veritatis quos
+      numquam ullam, laudantium, quibusdam iusto minus.
+    </div>
+  </>,
+  document.getElementById('root')
+);
